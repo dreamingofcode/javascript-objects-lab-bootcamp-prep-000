7df9ae6ae326cc9a9e027e9959ba485b4bf1caf9
cesar mota <cesarmota@ymail.com>
2019-01-21 18:51:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 object[key]="value";
 return object
 }
+
+object.assign({},object,{[key]:"value"});
+updateObjectWithKeyAndValue(obj, 'prop2', 2);
+}
